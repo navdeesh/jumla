4c74b4ef47763ac6bfb2c1e9ab1c4ab358360286
liteshpatil17 <liteshpatil17@gmail.com>
2019-02-12 13:28:35 +0530

    User model edited

--- jumla/videos_on_sale/models.py
+++ jumla/videos_on_sale/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-from django.db import models
-
 class Users(models.Model):
     user_id = models.CharField(max_length=10)
     user_name = models.CharField(max_length=50)
