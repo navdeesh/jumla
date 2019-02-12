14c44928ccf5ee2cff708d418245fdb0b73aa0b9
liteshpatil17 <liteshpatil17@gmail.com>
2019-02-12 13:51:15 +0530

    User model updated and created new models

--- jumla/videos_on_sale/models.py
+++ jumla/videos_on_sale/models.py
@@ -1,11 +1,35 @@
 from django.db import models
 
+
+
 class Users(models.Model):
-    user_id = models.CharField(max_length=10)
+    user_id = models.AutoField(primary_key=True)
     user_name = models.CharField(max_length=50)
     user_email = models.EmailField(max_length=100)
     user_password = models.CharField(max_length=32)
 
 
-    # artist = models.ForeignKey(Musician, on_delete=models.CASCADE)
+class ContentEntity(models.Model):
+    content_entity_id = models.AutoField(primary_key=True)
+    video_pack_foreign_key = models.ForeignKey('VideoPackEntity', on_delete=models.CASCADE)
+    video_foreign_key = models.ForeignKey('VideoEntity', on_delete=models.CASCADE)
+
+
+class VideoEntity(models.Model):
+    video_id = models.AutoField(primary_key=True)
+    video_title = models.CharField(max_length=300)
+    video_url = models.CharField(max_length=500)
+    genre_foreign_key = models.ForeignKey('GenreEntity', on_delete=models.CASCADE)
+
+
+class GenreEntity(models.Model):
+    genre_id = models.AutoField(primary_key=True)
+    genre_title = models.CharField(max_length=100)
+
+
+class VideoPackEntity(models.Model):
+    video_pack_id = models.AutoField(primary_key=True)
+    video_pack_name = models.CharField(max_length=100)
+    video_pack_content_id = models.ForeignKey('VideosInPack', on_delete=models.CASCADE)
+
 
