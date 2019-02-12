787d0f3fb038cba35031b544bdfa442aae9b60ff
liteshpatil17 <liteshpatil17@gmail.com>
2019-02-12 14:27:37 +0530

    admin file changed to add all the tables to admin panel

--- jumla/videos_on_sale/admin.py
+++ jumla/videos_on_sale/admin.py
@@ -1,13 +1,13 @@
 from django.apps import apps
 from django.contrib import admin
+from django.contrib.admin.sites import AlreadyRegistered
 
-# Register your models here.
-from .models import Users
 
-admin.site.register(Users)
 app_models = apps.get_app_config('videos_on_sale').get_models()
 for model in app_models:
     try:
         admin.site.register(model)
     except AlreadyRegistered:
         pass
+
+
