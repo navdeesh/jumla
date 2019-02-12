0ab1b15816c9e89d2e5ce7393e0f2ed567a3571f
Navdeesh Ahuja <navdeeshahuja@Navdeeshs-MacBook-Pro.local>
2019-02-12 14:20:37 +0530

    sign_in_api completed

--- jumla/videos_on_sale/views.py
+++ jumla/videos_on_sale/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse, JsonResponse
+from django.core.exceptions import ObjectDoesNotExist
 
 import json
 
@@ -12,14 +13,36 @@ def index(request):
 
 def sign_in(request):
     if request.method == "POST":
-        request_body=json.loads(request.body)
-        email = request_body['email']
-        password = request_body['password']
-        user = Users.objects.get(user_email=email)
-        return JsonResponse({
-            'success': True,
-            'message': 'Success'
-        })
+        try:
+            request_body=json.loads(request.body)
+            email = request_body['email']
+            password = request_body['password']
+            user = Users.objects.get(user_email=email)
+            if(user.user_password == password):
+                return JsonResponse({
+                    'success': True,
+                    'message': 'Login Successful'
+                })
+            else:
+                return JsonResponse({
+                    'success': False,
+                    'message': 'Wrong Password'
+                })
+        except ValueError:
+            return JsonResponse({
+                'success': False,
+                'message': 'JSON parse error'
+            })
+        except KeyError:
+            return JsonResponse({
+                'success': False,
+                'message': 'Please supply email and password in request body'
+            })
+        except ObjectDoesNotExist:
+            return JsonResponse({
+                'success': False,
+                'message': 'User does not exists'
+            })
     else:
         return JsonResponse({
             'success': False,
