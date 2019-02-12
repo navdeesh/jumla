8c13d1ac48d2de0fa66c2527c86de8cb40280717
liteshpatil17 <liteshpatil17@gmail.com>
2019-02-12 13:26:48 +0530

    User model created

--- jumla/videos_on_sale/models.py
+++ jumla/videos_on_sale/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+
+class Users(models.Model):
+    user_id = models.CharField(max_length=10)
+    user_name = models.CharField(max_length=50)
+    user_email = models.EmailField(max_length=100)
+    user_password = models.CharField(max_length=32)
+
+
+    # artist = models.ForeignKey(Musician, on_delete=models.CASCADE)
+
