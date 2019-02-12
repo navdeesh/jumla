4a9a33ded930264c0a9b1066e043cce15892a585
liteshpatil17 <liteshpatil17@gmail.com>
2019-02-12 15:11:05 +0530

    Pricing and Subsscribed model added

--- jumla/videos_on_sale/models.py
+++ jumla/videos_on_sale/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+import datetime
 
 
 class Users(models.Model):
@@ -36,4 +36,41 @@ class VideosInPack(models.Model):
     videos_in_pack_id = models.AutoField(primary_key=True)
     video_entity_foreign_key = models.ForeignKey('VideoEntity', on_delete=models.CASCADE)
     video_pack_entity_foreign_key = models.ForeignKey('VideoPackEntity', on_delete=models.CASCADE)
+
+
+class Pricing(models.Model):
+    user_foreign_key = models.ForeignKey('Users', on_delete=models.CASCADE)
+    content_foreign_key = models.ForeignKey('ContentEntity', on_delete=models.CASCADE)
+    pricing_daily_basis = models.IntegerField(max_length=6);
+    pricing_weekly_basis = models.IntegerField(max_length=6);
+    pricing_monthly_basis = models.IntegerField(max_length=6);
+    pricing_yearly_basis = models.IntegerField(max_length=6);
+
+
+class Subscribed(models.Model):
+    user_foreign_key = models.ForeignKey('Users', on_delete=models.CASCADE)
+    content_foreign_key = models.ForeignKey('ContentEntity', on_delete=models.CASCADE)
+    subscribed_amount = models.IntegerField(max_length=6)
+    subscribed_duration = models.CharField(max_length=10)
+    subscribed_start_date = models.DateTimeField(default=datetime.now, blank=True)
+
+    def is_subscription_expired(self):
+        if(str.lower(self.subscribed_duration)== "daily" and
+                self.subscribed_start_date < datetime.now() < (self.subscribed_start_date + datetime.timedelta(days=1)
+                )):
+            return True
+        elif (str.lower(self.subscribed_duration)== "weekly" and
+                self.subscribed_start_date < datetime.now() < (self.subscribed_start_date + datetime.timedelta(days=7)
+                )):
+            return True
+        elif (str.lower(self.subscribed_duration) == "month" and
+              self.subscribed_start_date < datetime.now() < (self.subscribed_start_date + datetime.timedelta(days=30)
+              )):
+            return True
+        elif (str.lower(self.subscribed_duration) == "yearly" and
+              self.subscribed_start_date < datetime.now() < (self.subscribed_start_date + datetime.timedelta(days=365)
+              )):
+            return True
+        else:
+            return False
 
