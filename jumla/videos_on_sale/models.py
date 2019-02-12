2ac3f29360fe27a48707f13c44792749a138d5a4
Navdeesh Ahuja <navdeeshahuja@Navdeeshs-MacBook-Pro.local>
2019-02-12 14:06:58 +0530

    removed db.sqlite3 and added it to gitignore

--- jumla/videos_on_sale/models.py
+++ jumla/videos_on_sale/models.py
@@ -9,27 +9,27 @@ class Users(models.Model):
     user_password = models.CharField(max_length=32)
 
 
-class ContentEntity(models.Model):
-    content_entity_id = models.AutoField(primary_key=True)
-    video_pack_foreign_key = models.ForeignKey('VideoPackEntity', on_delete=models.CASCADE)
-    video_foreign_key = models.ForeignKey('VideoEntity', on_delete=models.CASCADE)
+# class ContentEntity(models.Model):
+#     content_entity_id = models.AutoField(primary_key=True)
+#     video_pack_foreign_key = models.ForeignKey('VideoPackEntity', on_delete=models.CASCADE)
+#     video_foreign_key = models.ForeignKey('VideoEntity', on_delete=models.CASCADE)
 
 
-class VideoEntity(models.Model):
-    video_id = models.AutoField(primary_key=True)
-    video_title = models.CharField(max_length=300)
-    video_url = models.CharField(max_length=500)
-    genre_foreign_key = models.ForeignKey('GenreEntity', on_delete=models.CASCADE)
+# class VideoEntity(models.Model):
+#     video_id = models.AutoField(primary_key=True)
+#     video_title = models.CharField(max_length=300)
+#     video_url = models.CharField(max_length=500)
+#     genre_foreign_key = models.ForeignKey('GenreEntity', on_delete=models.CASCADE)
 
 
-class GenreEntity(models.Model):
-    genre_id = models.AutoField(primary_key=True)
-    genre_title = models.CharField(max_length=100)
+# class GenreEntity(models.Model):
+#     genre_id = models.AutoField(primary_key=True)
+#     genre_title = models.CharField(max_length=100)
 
 
-class VideoPackEntity(models.Model):
-    video_pack_id = models.AutoField(primary_key=True)
-    video_pack_name = models.CharField(max_length=100)
-    video_pack_content_id = models.ForeignKey('VideosInPack', on_delete=models.CASCADE)
+# class VideoPackEntity(models.Model):
+#     video_pack_id = models.AutoField(primary_key=True)
+#     video_pack_name = models.CharField(max_length=100)
+#     video_pack_content_id = models.ForeignKey('VideosInPack', on_delete=models.CASCADE)
 
 
--- jumla/videos_on_sale/views.py
+++ jumla/videos_on_sale/views.py
@@ -1,5 +1,10 @@
 from django.http import HttpResponse, JsonResponse
 
+import json
+
+from videos_on_sale.models import Users
+
+
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
@@ -7,6 +12,10 @@ def index(request):
 
 def sign_in(request):
     if request.method == "POST":
+        request_body=json.loads(request.body)
+        email = request_body['email']
+        password = request_body['password']
+        user = Users.objects.get(user_email=email)
         return JsonResponse({
             'success': True,
             'message': 'Success'
